dc7020f663d07c25539bf52f0713e1697fc2949e
amirmohammad <ramezaniamir.ar12@gmail.com>
2024-11-22 21:08:51 +0330

    feat: Add Class Customer

---
+++ src/Persistence/Persistence/Aggregates/Customers/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Aggregates.Customers
+{
+    public class Customer
+    {
+        
+        [MaxLength(50)]
+        public string FirstName { get; set; } = "";
+        [MaxLength(250)]
+        public string LastName { get; set; } = "";
+
+        public string ShippingAddress { get; set; } = "";
+        [MaxLength(15)]
+        public string NationalCode { get; set; } = "";
+        [MaxLength(11)]
+        public string Mobile { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string IsMobileVerified { get; set; } = "";
+
+        public string IsEmailVerified { get; set; } = "";
+
+        public string Password { get; set; } = "";
+        
+        
+    }
+}
