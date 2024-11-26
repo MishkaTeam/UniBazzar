ec7d5b345d7e18bcc8956b59ccdfbacf51fe5f6a
amirmohammad <ramezaniamir.ar12@gmail.com>
2024-11-26 19:02:58 +0330

    feat: create class shippingAddress..

--- src/Persistence/Persistence/Aggregates/Customers/Customer.cs
+++ src/Persistence/Persistence/Aggregates/Customers/Customer.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BuildingBlocks.Domain.Aggregates;
 
 namespace Persistence.Aggregates.Customers
 {
-    public class Customer
+    public class Customer: Entity
     {
         
-        [MaxLength(50)]
-        public string FirstName { get; set; } = "";
-        [MaxLength(250)]
-        public string LastName { get; set; } = "";
+        
+        public string FirstName { get; set; } 
+        
+        public string LastName { get; set; } 
 
-        public string ShippingAddress { get; set; } = "";
-        [MaxLength(15)]
-        public string NationalCode { get; set; } = "";
-        [MaxLength(11)]
-        public string Mobile { get; set; } = "";
+        public ShippingAddress fullAddress { get; set; } 
+       
+        public string NationalCode { get; set; } 
+        
+        public string Mobile { get; set; } 
 
-        public string Email { get; set; } = "";
+        public string Email { get; set; } 
 
-        public string IsMobileVerified { get; set; } = "";
+        public string IsMobileVerified { get; set; } 
 
-        public string IsEmailVerified { get; set; } = "";
+        public string IsEmailVerified { get; set; } 
 
-        public string Password { get; set; } = "";
+        public string Password { get; set; } 
         
         
     }
---
+++ src/Persistence/Persistence/Aggregates/Customers/ShippingAddress.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Aggregates.Customers
+{
+    public class ShippingAddress: Entity
+    {
+        public string Country { get; set; }
+        public string province { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+
+    }
+}
+//کشور - استان - شهر - آدرس - پلاک و کد پستی
