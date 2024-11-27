2394943efc15317fd2cbbcce0bd40f8ade92be2e
amirmohammad <ramezaniamir.ar12@gmail.com>
2024-11-27 20:01:11 +0330

    Merge Conflict

---
+++ src/Persistence/Persistence/Aggregates/Customers/Customer.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.Domain.Aggregates;
+
+namespace Persistence.Aggregates.Customers
+{
+    public class Customer: Entity
+    {
+        
+        
+        public Customer()
+        {
+            shippingAddresses = new List<ShippingAddress>();
+        }
+        
+        public string FirstName { get; set; } 
+        
+        public string LastName { get; set; } 
+
+        public List<ShippingAddress> shippingAddresses { get; set; } 
+       
+
+        public string NationalCode { get; set; } 
+        
+        public string Mobile { get; set; } 
+
+        public string Email { get; set; } 
+
+        public string IsMobileVerified { get; set; } 
+
+        public string IsEmailVerified { get; set; } 
+
+        public string Password { get; set; } 
+        
+        
+    }
+}
