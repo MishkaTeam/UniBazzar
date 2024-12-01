c109230691805d1f3b6e96b79bfb193eb40a20fe
amirmohammad <ramezaniamir.ar12@gmail.com>
2024-12-01 22:47:56 +0330

    feat: create shipingadresss + add fix..

--- src/Core/Domain/Aggregates/Customers/Customer.cs
+++ src/Core/Domain/Aggregates/Customers/Customer.cs
@@ -1,14 +1,17 @@
 ﻿using BuildingBlocks.Domain.Aggregates;
-
+using Framework.DataType;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
 namespace Domain.Aggregates.Customers
 {
     public class Customer : Entity
     {
+
         public Customer()
         {
             // FOR EF!
         }
-
         public string FirstName { get; private set; }
 
         public string LastName { get; private set; }
@@ -26,39 +29,47 @@ namespace Domain.Aggregates.Customers
         public string IsEmailVerified { get; private set; }
 
         public string Password { get; private set; }
+        
+
+        public void shippingAddress(string country, string province, string city, string address, string postalCode)
+        {
+
+            var Adress = ShippingAddress.create(country, province, city, address, postalCode);
+            shippingAddresses.Add(Adress);
+        }
 
         public static Customer register(string firstName, string lastName, string mobile, string password)
         {
+
             var customer = new Customer(firstName, lastName, mobile, password)
             {
-                FirstName = ValidateFirstname(firstName),
-
+                //FirstName = ValidateFirstname(firstName),
+                FirstName = firstName.Fix(),
+                LastName = lastName.Fix(),
             };
+
             return customer;
         }
         public void Update(string firstName, string lastName, string mobile, string password)
         {
-            FirstName = ValidateFirstname(firstName);
-            LastName = lastName;
+            FirstName = firstName.Fix();
+            LastName = lastName.Fix();
             Mobile = mobile;
             Password = password;
 
         }
-        private static string ValidateFirstname(string firstname)
-        {
-            if (string.IsNullOrWhiteSpace(firstname))
-            {
-                //var message=string.Format(Resources.Messages.Validations.GreaterThan,Resources.DataDictionary.)
-                throw new ArgumentException("Firstname cannot be null or empty.");
-            }
-
-            return firstname.Trim();
-           
-
-        }
-
+        //private static string ValidateFirstname(string firstname)
+        //{
+        //    if (string.IsNullOrWhiteSpace(firstname))
+        //    {
+        //        //var message=string.Format(Resources.Messages.Validations.GreaterThan,Resources.DataDictionary.)
+        //        throw new ArgumentException("Firstname cannot be null or empty.");
+        //    }
+        //    return firstname.Trim();
+        //}
         private Customer(string firstname, string lastName, string mobile, string password)
         {
+            
             shippingAddresses = new List<ShippingAddress>();
             FirstName = firstname;
             LastName = lastName;
--- src/Core/Domain/Aggregates/Customers/ShippingAddress.cs
+++ src/Core/Domain/Aggregates/Customers/ShippingAddress.cs
@@ -1,19 +1,34 @@
 ﻿using BuildingBlocks.Domain.Aggregates;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Framework.DataType;
 
 namespace Domain.Aggregates.Customers
 {
     public class ShippingAddress : Entity
     {
+
         public string Country { get; set; }
-        public string province { get; set; }
+        public string Province { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
         public string PostalCode { get; set; }
 
+        public static ShippingAddress create(string country, string province, string city, string address, string postalCode)
+        {
+            var shippingAddress = new ShippingAddress(country, province, city, address, postalCode)
+            {
+                Country = country.Fix(),
+                Province = country.Fix(),
+                City = city.Fix(),
+                Address = address.Fix(),
+            };
+            return shippingAddress;
+        }
+        private ShippingAddress(string country, string province, string city, string address, string postalCode)
+        {
+            Country = country;
+            Province = province;
+            City = city;
+            Address = address;
+        }
     }
 }
