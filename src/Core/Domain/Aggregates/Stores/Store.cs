032d37be5942b97a02c3e0786d1007b61b346627
SajadRmg <rmgsajad@gmai.com>
2024-12-06 01:49:06 -0800

    delete phone and adress and fix Ctor

--- src/Core/Domain/Aggregates/Stores/Store.cs
+++ src/Core/Domain/Aggregates/Stores/Store.cs
@@ -8,107 +8,66 @@ using System.Threading.Tasks;
 
 namespace Domain.Aggregates.Stores
 {
-    public class Store : IsEntityHasVersionControl, IEntityHasUpdateInfo, IEntityHasTenant, IEntityHasOwner
+    public class Store :  IEntityHasUpdateInfo
     {
         public Store()
         {
             //For EF 
         }
-        private Store(string name, string culture, string logo, string address, string telephone,bool isActive)
+        private Store(string name, string culture, string logo, bool isActive)
         {
             Name = name;
             Culture = culture;
             Logo = logo;
-            Address = address;
-            Telephone = telephone;
             Id=Guid.NewGuid();
-            IsActice = isActive;
+            IsActivce = isActive;
         }
         public Guid Id { get; private set; } 
         public string Name { get;private set; }
         public string Culture { get;private set; }
-        public string Address { get;private set; }
+       
         public string Logo { get;private set; }
-        public string Telephone { get;private set; }
-        public bool IsActice { get; private set; }
-        public int Ordering { get; }
-        public long InsertDateTime { get; }
-        public Guid InsertedByDate { get; }
-        void SetInserDateTime() { }
-        void SetInserBy() { }
 
+        public bool IsActivce { get; private set; }
+        public int Ordering { get; private set; }
+        public long UpdateDateTime { get; private set; }
+        public Guid UpdatedBy { get; private set; }
 
-        public int Version => throw new NotImplementedException();
+      
 
-        public long UpdateDateTime => throw new NotImplementedException();
-
-        public Guid UpdatedBy => throw new NotImplementedException();
-
-        public Guid TenantId => throw new NotImplementedException();
-
-        public Guid OwnerId => throw new NotImplementedException();
-
-        private static string ValidateTelephone(string tel)
+    
+        public static Store Create(string name, string culture, string logo,bool isActive)
         {
-            if (tel.Length!=14)
+            var store = new Store(name, culture, logo,isActive)
             {
-                var message = string.Format(Resources.Messages.Validations.CellPhoneNumber, Resources.DataDictionary.CellPhonenumber);
-
-                throw new ArgumentException(message: message);
-            }
-
-            return tel;
-        }
-        public static Store Create(string name, string culture, string logo,string address,string telephone,bool isActive)
-        {
-            var store = new Store(name, culture, logo,address,telephone,isActive)
-            {
-                Telephone = ValidateTelephone(telephone)
+                
             };
            
 
             return store;
         }
-        public void Update (string name, string culture, string logo, string address, string telephone,bool isActive)
+        public void Update (string name, string culture, string logo,bool isActive)
         {
 
             Name = name;
             Culture = culture;
             Logo = logo;
-            Address = address;
-            Telephone = telephone;
-            IsActice=isActive;
+            IsActivce=isActive;
+
+        }
+
+      
+
+        public void SetUpdateBy(Guid Id)
+        {
+            UpdatedBy = Id; 
 
         }
 
         public void SetUpdateDateTime()
         {
-            throw new NotImplementedException();
+            UpdateDateTime = DateTimeUtility.GetCurrentUnixUTCTimeSeconds();
         }
 
-        public void SetUpdateBy(Guid Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetTenant(Guid tenantId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Guid GetTenant()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetOwner(Guid ownerId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Guid GetOwner()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
