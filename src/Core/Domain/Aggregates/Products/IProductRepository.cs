14cbd68ab5bae90190c7cef612d7c3f2e3117811
MohammadSadeghAb <mohammadsadegh.ab@gmail.com>
2024-12-20 18:15:46 +0330

    feat: Add IProductRepository

---
+++ src/Core/Domain/Aggregates/Products/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Aggregates.Products.ProductImages;
+
+namespace Domain.Aggregates.Products;
+
+public interface IProductRepository
+{
+    void AddProductImage(ProductImage productImage);
+    Task<ProductImage> GetProductImageAsync(Guid id);
+    Task<List<ProductImage>> GetAllProductImagesAsync();
+    Task<List<ProductImage>> GetImageByProductIdAsync(Guid id);
+    void Remove(ProductImage productImage);
+}
