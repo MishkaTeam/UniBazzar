df4e815f16c5a735b2428088804905951d916bf0
amirmohammad <ramezaniamir.ar12@gmail.com>
2024-12-26 13:45:39 +0330

    feat: Add Class Customer + Add interface Customer..

---
+++ src/Core/Domain/Aggregates/Customers/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Aggregates.Units;
+
+namespace Domain.Aggregates.Customers;
+
+public interface ICustomerRepository
+{
+    void AddCustomer(Customer entity);
+    Task<List<Customer>> GetAllCustomerAsync();
+    Task<Customer> GetCustomerAsync(Guid id);
+    void Remove(Customer entity);
+}
