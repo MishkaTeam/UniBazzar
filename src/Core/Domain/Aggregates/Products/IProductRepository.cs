46cf60432c2e17b8d9abc83d74a604e6ab22bc88
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2024-12-27 15:56:44 +0330

    [Domain] Add IProductRepository and IProductFeatureRepository

---
+++ src/Core/Domain/Aggregates/Products/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Aggregates.Products.ProductFeatures;
+
+namespace Domain.Aggregates.Products;
+
+public interface IProductRepository : IProductFeatureRepository
+{
+	void AddProduct(Product entity);
+	Task<List<Product>> GetAllProductsAsync();
+	Task<Product> GetProductAsync(Guid id);
+	void RemoveProduct(Product entity);
+}
---
+++ src/Core/Domain/Aggregates/Products/ProductFeatures/IProductFeatureRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Aggregates.Products.ProductFeatures;
+
+public interface IProductFeatureRepository
+{
+	void AddProductFeature(ProductFeature entity);
+	Task<List<ProductFeature>> GetAllProductFeaturesAsync(Guid productId);
+	Task<ProductFeature> GetProductFeatureAsync(Guid id);
+	void RemoveProductFeature(ProductFeature entity);
+}
