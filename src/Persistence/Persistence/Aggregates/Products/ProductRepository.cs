4349b3b2d8555ec4f9eb41f78364af4112066541
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2024-12-27 15:59:25 +0330

    [Persistence] Add ProductRepository and ProductRepository.ProductFeature

---
+++ src/Persistence/Persistence/Aggregates/Products/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Aggregates.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Aggregates.Products;
+
+public partial class ProductRepository
+	(UniBazzarContext uniBazzarContext) : IProductRepository
+{
+	public void AddProduct(Product entity)
+	{
+		uniBazzarContext.Add(entity);
+	}
+
+	public Task<List<Product>> GetAllProductsAsync()
+	{
+		return uniBazzarContext.Products
+							   .Include(x => x.ActivePriceList)
+							   .Include(x => x.Unit)
+							   //.Include(x => x.BrandId)
+							   .Include(x => x.Category)
+							   .Include(x => x.Store)
+							   .ToListAsync();
+	}
+
+	public async Task<Product> GetProductAsync(Guid id)
+	{
+		var product = await uniBazzarContext.Products
+						.FirstOrDefaultAsync(x => x.Id == id);
+
+		return product ?? new Product();
+	}
+
+	public void RemoveProduct(Product entity)
+	{
+		uniBazzarContext.Remove(entity);
+	}
+}
