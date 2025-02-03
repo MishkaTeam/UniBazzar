40fd672b537377de2344a78393306daf92a58a7e
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-02-04 01:30:12 +0330

    [Domain] Add ICategoryRepository and FIx bugs

---
+++ src/Core/Domain/Aggregates/Categories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Aggregates.Categories;
+
+public interface ICategoryRepository
+{
+	Task AddCategoryAsync(Category entity);
+	Task<Category> GetCategoryAsync(Guid id);
+	Task<List<Category>> GetAllCategoriesAsync();
+	Task<List<Category>> GetSubCategoriesAsync(Guid parentId);
+	void RemoveCategory(Category entity);
+}
