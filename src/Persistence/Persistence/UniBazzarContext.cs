9e99fcb6246fc6947e1138b28de4ceb0e94882e7
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-02-04 00:35:31 +0330

    [Persistence] Add CategoryRepository and Category in Context

--- src/Persistence/Persistence/UniBazzarContext.cs
+++ src/Persistence/Persistence/UniBazzarContext.cs
@@ -1,8 +1,9 @@
-﻿using Domain.Aggregates.Products.ProductImages;
-using Domain.Aggregates.Products.ProductPriceLists;
+﻿using Domain.Aggregates.Categories;
+using Domain.Aggregates.Customers;
 using Domain.Aggregates.Products;
 using Domain.Aggregates.Products.ProductFeatures;
-using Domain.Aggregates.Customers;
+using Domain.Aggregates.Products.ProductImages;
+using Domain.Aggregates.Products.ProductPriceLists;
 using Domain.Aggregates.ShippingAddress;
 using Domain.Aggregates.Units;
 using Microsoft.EntityFrameworkCore;
@@ -11,18 +12,22 @@ namespace Persistence;
 
 public class UniBazzarContext : DbContext
 {
-    public UniBazzarContext(DbContextOptions options) : base(options)
-    {
-        Database.EnsureCreated();
-    }
+	public UniBazzarContext(DbContextOptions options) : base(options)
+	{
+		Database.EnsureCreated();
+	}
 
-    public DbSet<Unit> Units { get; set; }
-    public DbSet<ProductImage> ProductImages { get; set; }
-    public DbSet<ProductPriceList> ProductPriceLists { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<ProductFeature> ProductFeatures { get; set; }
 
-    public DbSet<Customer> customers { get; set; }
+	public DbSet<Unit> Units { get; set; }
 
-    public DbSet<ShippingAddress> shippingAddresses { get; set; }
+	public DbSet<Product> Products { get; set; }
+	public DbSet<ProductImage> ProductImages { get; set; }
+	public DbSet<ProductFeature> ProductFeatures { get; set; }
+	public DbSet<ProductPriceList> ProductPriceLists { get; set; }
+
+	public DbSet<Category> Categories { get; set; }
+
+	public DbSet<Customer> customers { get; set; }
+	public DbSet<ShippingAddress> shippingAddresses { get; set; }
+
 }
