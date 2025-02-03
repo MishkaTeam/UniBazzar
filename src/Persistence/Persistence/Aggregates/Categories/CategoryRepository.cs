436f7a1b629650fc6e02562b3d667159c4550a63
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-02-04 01:31:12 +0330

    [Persistence] Add CategoryRepository and Category in Context

---
+++ src/Persistence/Persistence/Aggregates/Categories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Aggregates.Categories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Aggregates.Categories;
+
+public class CategoryRepository
+	(UniBazzarContext uniBazzarContext) : ICategoryRepository
+{
+	public async Task AddCategoryAsync(Category entity)
+	{
+		await uniBazzarContext.AddAsync(entity);
+	}
+
+	public async Task<List<Category>> GetAllCategoriesAsync()
+	{
+		return await uniBazzarContext.Categories.ToListAsync();
+	}
+
+	public async Task<Category> GetCategoryAsync(Guid id)
+	{
+		var category = await uniBazzarContext.Categories
+						.FirstOrDefaultAsync(x => x.Id == id);
+
+		return category ?? new Category();
+	}
+
+	public async Task<List<Category>> GetSubCategoriesAsync(Guid parentId)
+	{
+		return await uniBazzarContext.Categories
+					.Where(x => x.ParentId == parentId)
+					.ToListAsync();
+	}
+
+	public void RemoveCategory(Category entity)
+	{
+		uniBazzarContext.Remove(entity);
+	}
+}
