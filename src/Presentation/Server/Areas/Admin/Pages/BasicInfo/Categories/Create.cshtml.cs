78190c25bb403c07afeab4fa31d72aa2186ac855
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-02-04 00:37:46 +0330

    [Server] Add Index and Create Category Pages

---
+++ src/Presentation/Server/Areas/Admin/Pages/BasicInfo/Categories/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Server.Areas.Admin.Pages.BasicInfo.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
