7d26dfad43272b4201922ff8d602e44939725dde
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-02-04 01:31:37 +0330

    [Application] Add CategoriesApplication and ViewModels

---
+++ src/Core/Application/Aggregates/Categories/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Aggregates.Categories.ViewModels;
+
+public class CategoryViewModel : CreateCategoryViewModel
+{
+	[Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.Id))]
+	public Guid Id { get; set; }
+}
---
+++ src/Core/Application/Aggregates/Categories/ViewModels/CreateCategoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Aggregates.Categories.ViewModels;
+
+public class CreateCategoryViewModel
+{
+	public CreateCategoryViewModel()
+	{
+		IconClass = "bi bi-box-fill";
+	}
+
+
+	[Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.Name))]
+	public string Name { get; set; }
+
+	[Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.Parent))]
+	public Guid? ParentId { get; set; }
+
+	[Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.IconClass))]
+	public string IconClass { get; set; }
+}
