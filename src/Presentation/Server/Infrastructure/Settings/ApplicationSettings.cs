ce7efdc20ab971477001732d6835d06882b8c536
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-02-13 22:33:50 +0330

    [Server] Add CultureSettings

--- src/Presentation/Server/Infrastructure/Settings/ApplicationSettings.cs
+++ src/Presentation/Server/Infrastructure/Settings/ApplicationSettings.cs
@@ -2,27 +2,34 @@
 
 public class ApplicationSettings : object
 {
-    public static readonly string KeyName = nameof(ApplicationSettings);
+	public static readonly string KeyName = nameof(ApplicationSettings);
 
-    public ApplicationSettings() : base()
-    {
-        ToastSettings =
-            new ToastSettings();
-    }
+	public ApplicationSettings() : base()
+	{
+		ToastSettings =
+			new ToastSettings();
 
-    // **********
-    public string? Version { get; set; }
-    // **********
+		CultureSettings =
+			new CultureSettings();
+	}
 
-    // **********
-    public string? MasterPassword { get; set; }
-    // **********
+	// **********
+	public string? Version { get; set; }
+	// **********
 
-    // **********
-    public string[]? ActivationKeys { get; set; }
-    // **********
+	// **********
+	public string? MasterPassword { get; set; }
+	// **********
 
-    // **********
-    public ToastSettings ToastSettings { get; set; }
-    // **********
+	// **********
+	public string[]? ActivationKeys { get; set; }
+	// **********
+
+	// **********
+	public ToastSettings ToastSettings { get; set; }
+	// **********
+
+	// **********
+	public CultureSettings CultureSettings { get; set; }
+	// **********
 }
---
+++ src/Presentation/Server/Infrastructure/Settings/CultureSettings.cs
@@ -0,0 +1,31 @@
+﻿namespace Server.Infrastructure.Settings;
+
+public class CultureSettings
+{
+	public CultureSettings()
+	{
+		DefaultCulture = new CultureData()
+		{
+			Name = "فارسی - ایران",
+			Culture = "fa-IR",
+		};
+
+		SupportedCulture =
+			new List<CultureData>();
+	}
+
+
+	// **********
+	public CultureData DefaultCulture { get; set; }
+	// **********
+
+	// **********
+	public List<CultureData> SupportedCulture { get; set; }
+	// **********
+
+	public class CultureData
+	{
+		public string Name { get; set; }
+		public string Culture { get; set; }
+	}
+}
