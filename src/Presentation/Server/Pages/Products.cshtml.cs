8d39983bb0569438ee85c561e9691ff1660954d1
MohammadSadeghAb <113609836+MohammadSadeghAb@users.noreply.github.com>
2025-03-09 21:32:10 +0330

    refactor: Show Products from Database (#39)

--- src/Presentation/Server/Pages/Products.cshtml.cs
+++ src/Presentation/Server/Pages/Products.cshtml.cs
@@ -1,12 +1,25 @@
+using Application.Aggregates.Products;
+using Application.Aggregates.Products.ProductImages.ViewModel;
+using Application.Aggregates.Products.ProductPriceLists.ViewModels;
+using Application.Aggregates.Products.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Server.Pages
 {
-    public class ProductsModel : PageModel
+    public class ProductsModel(ProductsApplication productsApplication) : PageModel
     {
-        public void OnGet()
+        public List<ProductViewModel> ProductsViewModel { get; set; } = [];
+        public List<ProductImageViewModel> ProductImageViewModel { get; set; } = [];
+        public List<ProductPriceListViewModel> ProductPriceListViewModel { get; set; } = [];
+
+        public async Task OnGetAsync()
         {
+            ProductsViewModel = await productsApplication.GetProducts();
+
+            ProductImageViewModel = await productsApplication.GetAllProductImagesAsync();
+
+            ProductPriceListViewModel = await productsApplication.GetAllProductPriceListAsync();
         }
     }
 }
