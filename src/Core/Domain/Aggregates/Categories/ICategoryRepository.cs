25db82788ffc83f23516362613cd6f4240c3f7b3
amirmohammad <ramezaniamir.ar12@gmail.com>
2025-03-13 15:06:57 +0330

    feat: Add MenuCategory...Edit CategoryApplication..

--- src/Core/Domain/Aggregates/Categories/ICategoryRepository.cs
+++ src/Core/Domain/Aggregates/Categories/ICategoryRepository.cs
@@ -2,11 +2,12 @@
 
 public interface ICategoryRepository
 {
-	Task AddCategoryAsync(Category entity);
-	Task<Category?> GetCategoryAsync(Guid id);
-	Task<List<Category>> GetAllCategoriesAsync();
-	Task<List<Category>> GetRootCategoriesAsync();
-	Task<List<Category>> GetSubCategoriesAsync(Guid parentId);
-	Task<int> GetSubCategoriesCountAsync(Guid parentId);
-	void RemoveCategory(Category entity);
+    Task AddCategoryAsync(Category entity);
+    Task<Category?> GetCategoryAsync(Guid id);
+    Task<List<Category>> GetAllCategoriesAsync();
+    Task<List<Category>> GetRootCategoriesAsync();
+    //Task<List<Category>> GetMenuCategoriesAsync();
+    Task<List<Category>> GetSubCategoriesAsync(Guid parentId);
+    Task<int> GetSubCategoriesCountAsync(Guid parentId);
+    void RemoveCategory(Category entity);
 }
