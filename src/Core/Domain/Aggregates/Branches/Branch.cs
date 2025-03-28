6ba0e9487fe928ddeefdad7fa467d58853aa13b4
SajadRmg <rmgsajad@gmai.com>
2025-03-28 06:45:47 -0700

    Create Branche

---
+++ src/Core/Domain/Aggregates/Branches/Branch.cs
@@ -0,0 +1,40 @@
+﻿using Entity = Domain.BuildingBlocks.Aggregates.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.Branches
+{
+	public class Branch : Entity
+	{
+		public Branch() 
+		{
+			//For EF!
+		}
+		
+		public string Title {  get; private set; }
+		public Guid StoreID { get; private set; }
+
+		public static Branch Create(string title, Guid storeID)
+		{
+			var branch = new Branch(title, storeID)
+			{
+
+			};
+			return branch;
+		}
+		private Branch(string title, Guid storeID)
+		{
+			Title = title;
+			StoreID = storeID;
+		}
+		public void Update(string title, Guid storeID)
+		{
+			Title = title;
+			StoreID = storeID;
+		
+		}
+	}
+}
