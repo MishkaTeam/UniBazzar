685c7d4fe7e517bf7ba2d58691c8e4f6da749a8c
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-04-26 22:01:06 +0330

    [Application] Add Pos ViewModel and some changes

--- src/Core/Application/Aggregates/Customer/CustomerApplication.cs
+++ src/Core/Application/Aggregates/Customer/CustomerApplication.cs
@@ -1,75 +1,92 @@
-﻿using Application.Aggregates.Units.ViewModels;
+﻿using Application.Aggregates.Customer.ViewModels;
 using Domain;
 using Domain.Aggregates.Customers;
 using Mapster;
 
-namespace Application.Aggregates.Customer
+namespace Application.Aggregates.Customer;
+
+public class CustomerApplication(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
 {
-    public class CustomerApplication(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
-    {
-        public async Task<CreateCustomerViewModel> CreateAsync(CreateCustomerViewModel viewModel)
-        {
-            var entity = Domain.Aggregates.Customers.Customer.Register
-                (
-                viewModel.FirstName,
-                viewModel.LastName,
-                viewModel.NationalCode,
-                viewModel.Mobile,
-                viewModel.Password,
-                viewModel.Email
-                );
-            customerRepository.AddCustomer(entity);
-            await unitOfWork.CommitAsync();
-            return entity.Adapt<CreateCustomerViewModel>();
-        }
-        public async Task<List<UpdateCustomerViewModel>> GetAllCustomer()
-        {
-            var customers = await customerRepository.GetAllCustomersAsync();
-            return customers.Adapt<List<UpdateCustomerViewModel>>();
-        }
+	public async Task<CreateCustomerViewModel> CreateAsync(CreateCustomerViewModel viewModel)
+	{
+		var entity = Domain.Aggregates.Customers.Customer.Register
+			(
+			viewModel.FirstName,
+			viewModel.LastName,
+			viewModel.NationalCode,
+			viewModel.Mobile,
+			viewModel.Password,
+			viewModel.Email
+			);
+		customerRepository.AddCustomer(entity);
+		await unitOfWork.CommitAsync();
+		return entity.Adapt<CreateCustomerViewModel>();
+	}
 
-        public async Task<UpdateCustomerViewModel> GetCustomerAsync(Guid id)
-        {
-            var customer = await customerRepository.GetCustomerAsync(id);
+	public async Task<CreateCustomerViewModelPos> CreateAsync(CreateCustomerViewModelPos viewModel)
+	{
+		var customer = Domain.Aggregates.Customers.Customer.Register(
+			firstName: null,
+			viewModel.LastName,
+			viewModel.NationalCode,
+			viewModel.Mobile,
+			password: null,
+			email: null
+			);
 
-            if (customer == null || customer.Id == Guid.Empty)
-            {
-                throw new Exception(Resources.Messages.Errors.NotFound);
-            }
-            return customer.Adapt<UpdateCustomerViewModel>();
-        }
+		customerRepository.AddCustomer(customer);
+		await unitOfWork.CommitAsync();
 
-        public async Task<UpdateCustomerViewModel> UpdateAsync(UpdateCustomerViewModel updateViewModel)
-        {
-            var entity = await customerRepository.GetCustomerAsync(updateViewModel.Id);
+		return customer.Adapt<CreateCustomerViewModelPos>();
+	}
 
-            if (entity == null || entity.Id == Guid.Empty)
-            {
-                throw new Exception(Resources.Messages.Errors.NotFound);
-            }
+	public async Task<List<UpdateCustomerViewModel>> GetAllCustomer()
+	{
+		var customers = await customerRepository.GetAllCustomersAsync();
+		return customers.Adapt<List<UpdateCustomerViewModel>>();
+	}
 
-            entity.Update(
-                updateViewModel.FirstName,
-                updateViewModel.LastName,
-                updateViewModel.NationalCode,
-                updateViewModel.Mobile
-                );
+	public async Task<UpdateCustomerViewModel> GetCustomerAsync(Guid id)
+	{
+		var customer = await customerRepository.GetCustomerAsync(id);
 
-            await unitOfWork.CommitAsync();
-            return entity.Adapt<UpdateCustomerViewModel>();
-        }
+		if (customer == null || customer.Id == Guid.Empty)
+		{
+			throw new Exception(Resources.Messages.Errors.NotFound);
+		}
+		return customer.Adapt<UpdateCustomerViewModel>();
+	}
 
-        public async Task DeleteAsync(Guid id)
-        {
-            var entity = await customerRepository.GetCustomerAsync(id);
+	public async Task<UpdateCustomerViewModel> UpdateAsync(UpdateCustomerViewModel updateViewModel)
+	{
+		var entity = await customerRepository.GetCustomerAsync(updateViewModel.Id);
 
-            if (entity == null || entity.Id == Guid.Empty)
-            {
-                throw new Exception(Resources.Messages.Errors.NotFound);
-            }
+		if (entity == null || entity.Id == Guid.Empty)
+		{
+			throw new Exception(Resources.Messages.Errors.NotFound);
+		}
 
-            customerRepository.Remove(entity);
-            await unitOfWork.CommitAsync();
-        }
-    }
+		entity.Update(
+			updateViewModel.FirstName,
+			updateViewModel.LastName,
+			updateViewModel.NationalCode,
+			updateViewModel.Mobile
+			);
+
+		await unitOfWork.CommitAsync();
+		return entity.Adapt<UpdateCustomerViewModel>();
+	}
+
+	public async Task DeleteAsync(Guid id)
+	{
+		var entity = await customerRepository.GetCustomerAsync(id);
+
+		if (entity == null || entity.Id == Guid.Empty)
+		{
+			throw new Exception(Resources.Messages.Errors.NotFound);
+		}
+
+		customerRepository.Remove(entity);
+		await unitOfWork.CommitAsync();
+	}
 }
--- src/Core/Application/Aggregates/Customer/UpdateCustomerViewModel.cs
+++ src/Core/Application/Aggregates/Customer/UpdateCustomerViewModel.cs
@@ -1,25 +1,16 @@
-﻿using Domain.Aggregates.Customers;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Application.Aggregates.Customer
+namespace Application.Aggregates.Customer;
+
+public class UpdateCustomerViewModel : CreateCustomerViewModel
 {
-    public class UpdateCustomerViewModel : CreateCustomerViewModel 
-    {
-        [Display(ResourceType = typeof(Resources.DataDictionary),
-                   Name = nameof(Resources.DataDictionary.Name))]
-        public string FirstName { get; set; }
+	public UpdateCustomerViewModel()
+	{
+	}
 
-        [Display(ResourceType = typeof(Resources.DataDictionary),
-           Name = nameof(Resources.DataDictionary.Family))]
-        public string LastName { get; set; }
 
-        [Display(ResourceType = typeof(Resources.DataDictionary),
-           Name = nameof(Resources.DataDictionary.NationalCode))]
-        public string NationalCode { get; set; }
-
-        [Display(ResourceType = typeof(Resources.DataDictionary),
-           Name = nameof(Resources.DataDictionary.Id))]
-        public Guid Id { get;  set; }
-
-    }
+	[Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.Id))]
+	public Guid Id { get; set; }
 }
