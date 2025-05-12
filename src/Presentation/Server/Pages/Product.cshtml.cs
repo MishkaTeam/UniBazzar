a33fae6978e9f4a665d1aed4342f467fc4cbb508
amirmohammad <ramezaniamir.ar12@gmail.com>
2025-05-12 20:42:44 +0330

    Add product in index..

--- src/Presentation/Server/Pages/Product.cshtml.cs
+++ src/Presentation/Server/Pages/Product.cshtml.cs
@@ -1,12 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
+using Application.Aggregates.Products;
+using Application.Aggregates.Products.ProductFeatures.ViewModels;
+using Application.Aggregates.Products.ProductImages.ViewModel;
+using Application.Aggregates.Products.ProductPriceLists.ViewModels;
+using Application.Aggregates.Products.ViewModels;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Server.Pages
+namespace Server.Pages;
+
+public class ProductModel(ProductsApplication productsApplication,
+                          ProductImagesApplication productImagesApplication,
+                          ProductFeaturesApplication productFeaturesApplication,
+                          ProductPriceListsApplication productPriceListsApplication) : PageModel
 {
-    public class ProductModel : PageModel
+
+    public ProductViewModel ViewModelProduct { get; set; }
+    public List<ProductImageViewModel> ViewModelProductImage { get; set; }
+    public List<ProductFeatureViewModel> ViewModelProductFeature { get; set; }
+    public ProductPriceListViewModel ViewModelProductPrice { get; set; }
+
+    public async Task OnGetAsync(Guid id)
     {
-        public void OnGet()
-        {
-        }
+        ViewModelProduct = await productsApplication.GetProductAsync(id);
+
+        ViewModelProductImage = await productImagesApplication.GetImageByProductIdAsync(id);
+
+        ViewModelProductFeature = await productFeaturesApplication.GetProductFeatures(id);
+
+        ViewModelProductPrice = await productPriceListsApplication.GetPriceByProductId(id);
     }
 }
