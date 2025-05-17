e96a5491febdff525b4ac93aaa88cdba15832c3d
Hussein Beygi <husseinbeygi@outlook.com>
2025-05-17 20:19:18 +0330

    feat: Category Menu

--- src/Presentation/Server/Pages/Index.cshtml.cs
+++ src/Presentation/Server/Pages/Index.cshtml.cs
@@ -10,22 +10,17 @@ using Server.Infrastructure;
 
 namespace Server.Pages
 {
-    public class IndexModel (CategoriesApplication categoriesApplication,
+    public class IndexModel (
                              ProductsApplication productsApplication,
                              ProductImagesApplication productImagesApplication,
                              ProductPriceListsApplication productPriceListsApplication) : PageModel
     {
-
-        public List<MenuCategoryViewModel> MenuCategory { get; set; } = [];
-
         public List<ProductViewModel> ProductsViewModel { get; set; } = [];
         public List<ProductImageViewModel> ProductImageViewModel { get; set; } = [];
         public List<ProductPriceListViewModel> ProductPriceListViewModel { get; set; } = [];
 
         public async Task OnGetAsync()
         {
-            MenuCategory = await categoriesApplication.GetMenuCategoriesAsync();
-
             ProductsViewModel = await productsApplication.GetProducts();
             ProductImageViewModel = await productImagesApplication.GetAllProductImagesAsync();
             ProductPriceListViewModel = await productPriceListsApplication.GetAllProductPriceListAsync();
---
+++ src/Presentation/Server/Pages/Shared/Components/SiteMenu/SiteMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Application.Aggregates.Categories;
+using Application.Aggregates.Categories.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Pages.Shared.Components.SiteMenu
+{
+    public class SiteMenuViewComponent(CategoriesApplication categoriesApplication) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string message)
+        {
+            var menuCategories = await categoriesApplication.GetMenuCategoriesAsync();
+
+            return View("Default", menuCategories);
+        }
+
+    }
+}
