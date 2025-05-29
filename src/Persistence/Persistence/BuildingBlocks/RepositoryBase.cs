bc04665d15436ae77a21e2fc07a669dd0acffc94
Sajjad <sajjad.doosty.83@gmail.com>
2025-05-29 18:52:51 +0330

    [Persistence] fix: fix OwnerId bug in RepositoryBase

--- src/Persistence/Persistence/BuildingBlocks/RepositoryBase.cs
+++ src/Persistence/Persistence/BuildingBlocks/RepositoryBase.cs
@@ -8,167 +8,170 @@ using Persistence.Extensions;
 
 namespace BuildingBlocks.Persistence;
 
-public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> 
-	where TEntity : class, IEntity
-	{
-		public RepositoryBase(UniBazzarContext context, IExecutionContextAccessor executionContext)
-		{
-			DatabaseContext = context ?? throw new ArgumentNullException("databaseContext");
-			ExecutionContext = executionContext;
-			DbSet = DatabaseContext.Set<TEntity>();
-		}
+public class RepositoryBase<TEntity> : IRepositoryBase<TEntity>
+    where TEntity : class, IEntity
+{
+    public RepositoryBase(UniBazzarContext context, IExecutionContextAccessor executionContext)
+    {
+        DatabaseContext = context ?? throw new ArgumentNullException("databaseContext");
+        ExecutionContext = executionContext;
+        DbSet = DatabaseContext.Set<TEntity>();
+    }
 
-		protected DbSet<TEntity> DbSet { get; }
+    protected DbSet<TEntity> DbSet { get; }
 
-		protected DbContext DatabaseContext { get; }
-		public IExecutionContextAccessor ExecutionContext { get; }
+    protected DbContext DatabaseContext { get; }
+    public IExecutionContextAccessor ExecutionContext { get; }
 
-		public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
-		{
-			if (entity == null)
-			{
-				throw new ArgumentNullException("entity");
-			}
+    public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
 
-			entity.IncreaseVersion();
+        entity.IncreaseVersion();
 
-			entity.SetInsertBy(ExecutionContext.UserId ?? throw new Exception("User Is Empty"));
+        entity.SetInsertBy(ExecutionContext.UserId ?? throw new Exception("User Is Empty"));
 
-			entity.SetOwner(ExecutionContext.UserId ?? throw new Exception("User Is Empty"));
+        if (entity.OwnerId == Guid.Empty)
+        {
+            entity.SetOwner(ExecutionContext.UserId ?? throw new Exception("User Is Empty"));
+        }
 
-			entity.SetStore(ExecutionContext.StoreId);
+        entity.SetStore(ExecutionContext.StoreId);
 
-			entity.SetInsertDateTime();
+        entity.SetInsertDateTime();
 
-			await DbSet.AddAsync(entity, cancellationToken);
-		}
+        await DbSet.AddAsync(entity, cancellationToken);
+    }
 
-		public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
-		{
-			if (entities == null)
-			{
-				throw new ArgumentNullException("entities");
-			}
+    public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+    {
+        if (entities == null)
+        {
+            throw new ArgumentNullException("entities");
+        }
 
-			await DbSet.AddRangeAsync(entities, cancellationToken);
-		}
+        await DbSet.AddRangeAsync(entities, cancellationToken);
+    }
 
-		public virtual async Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default)
-		{
-			TEntity entity2 = entity;
-			switch (entity)
-			{
-				case null:
-				throw new ArgumentNullException(paramName: nameof(entity));
+    public virtual async Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        TEntity entity2 = entity;
+        switch (entity)
+        {
+            case null:
+                throw new ArgumentNullException(paramName: nameof(entity));
 
-				case IEntityHasIsDeleted softDeletedEntity:
-				softDeletedEntity.Delete();
-				softDeletedEntity.SetDeleteDateTime();
-				await SoftDeleteAsync(entity, cancellationToken);
-				break;
+            case IEntityHasIsDeleted softDeletedEntity:
+                softDeletedEntity.Delete();
+                softDeletedEntity.SetDeleteDateTime();
+                await SoftDeleteAsync(entity, cancellationToken);
+                break;
 
-				default:
-				await Task.Run(delegate
-				{
-					DbSet.Remove(entity2);
-				}, cancellationToken);
-				break;
-			}
-		}
-		private async Task SoftDeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
-		{
-			DbSet.Update(entity);
-		}
-		public virtual async Task<bool> RemoveByIdAsync(Guid id, CancellationToken cancellationToken = default)
-		{
-			TEntity entity = await FindAsync(id, cancellationToken);
+            default:
+                await Task.Run(delegate
+                {
+                    DbSet.Remove(entity2);
+                }, cancellationToken);
+                break;
+        }
+    }
+    private async Task SoftDeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        DbSet.Update(entity);
+    }
+    public virtual async Task<bool> RemoveByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        TEntity entity = await FindAsync(id, cancellationToken);
 
-			await RemoveAsync(entity, cancellationToken);
+        await RemoveAsync(entity, cancellationToken);
 
-			return true;
-		}
+        return true;
+    }
 
-		public virtual async Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
-		{
-			if (entities == null)
-			{
-				throw new ArgumentNullException("entities");
-			}
+    public virtual async Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+    {
+        if (entities == null)
+        {
+            throw new ArgumentNullException("entities");
+        }
 
-			foreach (TEntity entity in entities)
-			{
-				await RemoveAsync(entity, cancellationToken);
-			}
-		}
+        foreach (TEntity entity in entities)
+        {
+            await RemoveAsync(entity, cancellationToken);
+        }
+    }
 
-		public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
-		{
+    public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
 
-			TEntity oldentity = await FindAsync(entity.Id, cancellationToken);
+        TEntity oldentity = await FindAsync(entity.Id, cancellationToken);
 
-			if (oldentity.Version > entity.Version)
-				throw new InvalidEntityVersionException();
+        if (oldentity.Version > entity.Version)
+            throw new InvalidEntityVersionException();
 
-			entity.SetVersionAndIncrease(oldentity.Version);
+        entity.SetVersionAndIncrease(oldentity.Version);
 
-			entity.SetUpdateBy(ExecutionContext.UserId ?? throw new Exception("User Is Empty"));
+        entity.SetUpdateBy(ExecutionContext.UserId ?? throw new Exception("User Is Empty"));
 
-			entity.SetUpdateDateTime();
+        entity.SetUpdateDateTime();
 
-			DbSet.Update(entity);
-		}
+        DbSet.Update(entity);
+    }
 
-		public virtual async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
-		{
-			return await DbSet
-							.StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
-							.AsNoTracking()
-							.ToListAsync(cancellationToken);
-		}
-		
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await DbSet
+                        .StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
+                        .AsNoTracking()
+                        .ToListAsync(cancellationToken);
+    }
 
-		public virtual async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate
-			, CancellationToken cancellationToken = default)
-		{
-			return await DbSet
-			.AsNoTracking()
-			.Where(predicate)
-			.ToListAsync(cancellationToken);
-		}
 
-		public virtual async Task<TEntity> FindAsync(Guid id, CancellationToken cancellationToken = default)
-		{
-			return await DbSet
-				.StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
-				.FirstOrDefaultAsync(x => x.Id.Equals(id), cancellationToken);
-		}
+    public virtual async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate
+        , CancellationToken cancellationToken = default)
+    {
+        return await DbSet
+        .AsNoTracking()
+        .Where(predicate)
+        .ToListAsync(cancellationToken);
+    }
 
-		public virtual async Task<IEnumerable<TEntity>> GetSomeAsync(Expression<Func<TEntity, bool>> predicate
-				, CancellationToken cancellationToken = default)
-		{
-			return await DbSet
-			.StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
-			.AsNoTracking()
-			.Where(predicate)
-			.ToListAsync(cancellationToken);
-		}
+    public virtual async Task<TEntity> FindAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await DbSet
+            .StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
+            .FirstOrDefaultAsync(x => x.Id.Equals(id), cancellationToken);
+    }
 
-		public virtual async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
-		{
-			return await FindAsync(id, cancellationToken);
-		}
+    public virtual async Task<IEnumerable<TEntity>> GetSomeAsync(Expression<Func<TEntity, bool>> predicate
+            , CancellationToken cancellationToken = default)
+    {
+        return await DbSet
+        .StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
+        .AsNoTracking()
+        .Where(predicate)
+        .ToListAsync(cancellationToken);
+    }
 
-		public virtual async Task SaveChangesAsync(CancellationToken cancellationToken = default)
-		{
-			await DatabaseContext.SaveChangesAsync(cancellationToken);
-		}
+    public virtual async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await FindAsync(id, cancellationToken);
+    }
 
-		public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
-		{
-			return await DbSet.
-			StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
-			.AsNoTracking()
-			.FirstOrDefaultAsync(predicate, cancellationToken);
-		}
+    public virtual async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await DatabaseContext.SaveChangesAsync(cancellationToken);
+    }
 
-	}
+    public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+    {
+        return await DbSet.
+        StoreFilter<TEntity>(tenantId: ExecutionContext.StoreId)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(predicate, cancellationToken);
+    }
+
+}
