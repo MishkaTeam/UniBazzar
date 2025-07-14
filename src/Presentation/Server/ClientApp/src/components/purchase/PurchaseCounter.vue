934e45d513b76c0efa89b6f96ef7e11fd716d32d
Hussein Beygi <husseinbeygi@outlook.com>
2025-07-15 00:09:40 +0330

    chagnes

---
+++ src/Presentation/Server/ClientApp/src/components/purchase/PurchaseCounter.vue
@@ -0,0 +1,96 @@
+﻿<template>
+    <div class="counter-box">
+        <i v-if="quantity <= 1" class="bi bi-trash icon-button"></i>
+        <i v-if="quantity > 1" @click="quantity--" class="bi bi-dash icon-button"></i>
+
+        <div class="d-flex flex-column align-items-center">
+            <input type="hidden" name="Quantity" :value="quantity" />
+            <div class="counter-number">{{ quantity }}</div>
+            <!-- <div class="counter-label">حداکثر</div> -->
+        </div>
+        <i @click="quantity++" class="bi bi-plus icon-button"></i>
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+    initialValue: {
+        type: Number,
+        default: 1
+    },
+    basketItemId: {
+        type: String,
+        default: ''
+    },
+    basketId: {
+        type: String,
+        default: ''
+    },
+    maxValue: {
+        type: Number,
+        default: 0
+    },
+    isDisabled: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const quantity = ref(props.initialValue)
+const basketId = ref(props.basketId)
+const basketItemId = ref(props.basketItemId)
+
+const postUrl = '/Purchase/Cart?handler=UpdateQuantity'
+
+watch(quantity, async (newVal) => {
+    await fetch(postUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ quantity: newVal, basketItemId: basketItemId.value, basketId: basketId.value })
+    }).then(res => {
+        var qun = quantity;
+        res.json().then(x => qun = x.quantity)
+        quantity.value = qun
+        // quantity = ref(res.json)
+    })
+})
+</script>
+
+<style scoped>
+.counter-box {
+    border: 1px solid #dee2e6;
+    border-radius: 12px;
+    padding: 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 160px;
+}
+
+.icon-button {
+    cursor: pointer;
+    color: #f44336;
+    font-size: 1.25rem;
+}
+
+.icon-button.disabled {
+    color: #ccc;
+    cursor: not-allowed;
+}
+
+.counter-number {
+    color: #f44336;
+    font-weight: bold;
+    font-size: 1.25rem;
+}
+
+.counter-label {
+    font-size: 0.8rem;
+    color: #999;
+    text-align: center;
+}
+</style>
--- src/Presentation/Server/ClientApp/src/main.js
+++ src/Presentation/Server/ClientApp/src/main.js
@@ -1,18 +1,34 @@
 import { createApp } from 'vue';
 import Counter from './components/Counter.vue';
+import PurchaseCounter from './components/purchase/PurchaseCounter.vue';
 
 // لیستی از تمام کامپوننت‌های موجود
 const components = {
-  'Counter': Counter
+  'Counter': Counter,
+  'PurchaseCounter' : PurchaseCounter
 };
 
+function getPropsFromDataset(dataset) {
+	const props = {};
+	for (const key in dataset) {
+		let value = dataset[key];
+		// Try parsing numbers and booleans
+		if (value === "true") value = true;
+		else if (value === "false") value = false;
+		else if (!isNaN(value)) value = Number(value);
+		props[key] = value;
+	}
+	return props;
+
+}
 // پیدا کردن تمام المنت‌هایی که قرار است میزبان کامپوننت باشند
 document.querySelectorAll('[data-vue-component]').forEach(el => {
   const componentName = el.dataset.vueComponent;
   const component = components[componentName];
   
-  if (component) {
-    createApp(component).mount(el);
+	if (component) {
+		const props = getPropsFromDataset(el.dataset);
+		createApp(component, props).mount(el);
   } else {
     console.error(`کامپوننت '${componentName}' یافت نشد.`);
   }
