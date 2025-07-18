91d0e7c85c0df8d467a4583a5dbda4c68d6382a0
Sajjad <sajjad.doosty.83@gmail.com>
2025-07-18 03:56:41 +0330

    [Persistence] add: HomeView repository

---
+++ src/Persistence/Persistence/Repositories/Aggregates/HomeViews/HomeViewsRepository.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Persistence;
+using Domain.Aggregates.Cms.HomeViews;
+using Domain.Aggregates.Cms.HomeViews.Data;
+
+namespace Persistence.Repositories.Aggregates.HomeViews;
+
+public class HomeViewsRepository
+    (UniBazzarContext context,
+    IExecutionContextAccessor executionContext)
+    : RepositoryBase<HomeView>(context, executionContext), IHomeViewRepository
+{
+}
