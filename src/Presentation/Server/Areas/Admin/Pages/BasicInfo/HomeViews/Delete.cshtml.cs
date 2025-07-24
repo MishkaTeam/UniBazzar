d610bfd89713be9d8eee89624e6b4840de2a5f3e
Sajjad <sajjad.doosty.83@gmail.com>
2025-07-24 18:31:41 +0330

    [Server] add: HomeView Pages (without Update Page)

---
+++ src/Presentation/Server/Areas/Admin/Pages/BasicInfo/HomeViews/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+using Application.Aggregates.HomeViews;
+using Application.Aggregates.HomeViews.ViewModels;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Areas.Admin.Pages.BasicInfo.HomeViews;
+
+public class DeleteModel
+    (HomeViewsApplication homeViewsApplication) : BasePageModel
+{
+    [BindProperty]
+    public HomeViewViewModel DeleteViewModel { get; set; } = new();
+
+    public async Task<IActionResult> OnGetAsync(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return RedirectToPage("Index");
+        }
+
+        DeleteViewModel =
+            (await homeViewsApplication.GetHomeViewAsync(id)).Data!;
+
+        if (DeleteViewModel == null)
+        {
+            return RedirectToPage("Index");
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await homeViewsApplication.DeleteHomeView(DeleteViewModel.Id);
+
+        return RedirectToPage("Index");
+    }
+}
