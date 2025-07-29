fd76bee6991c86b0402b2b6ffb23e333cc1b01c1
Sajjad <sajjad.doosty.83@gmail.com>
2025-07-29 20:14:55 +0330

    [Server] add: StorageService class

---
+++ src/Presentation/Server/Infrastructure/Services/StorageService.cs
@@ -0,0 +1,47 @@
+﻿using BuildingBlocks.Persistence;
+using Framework.DataType;
+using Framework.Storage;
+
+namespace Server.Infrastructure.Services;
+
+public class StorageService(
+    IExecutionContextAccessor executionContextAccessor,
+    IStorage storage)
+{
+    public async Task<ResultContract<string>> UploadImageAsync(IFormFile formFile, string prefix, string path)
+    {
+        var size =
+            formFile.Length;
+
+        // Check Image Length
+        if (size == 0)
+        {
+            var message =
+                "file is too large.";
+
+            return (ErrorType.FileWriteError, message);
+        }
+
+        using var memoryStream = new MemoryStream();
+
+        await formFile.CopyToAsync
+            (memoryStream).ConfigureAwait(false);
+
+        var fileType =
+            formFile.ContentType;
+
+        string objectKey =
+            $"{prefix}_IMG_{executionContextAccessor.StoreId}_{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}";
+
+        var folder =
+            $"{executionContextAccessor.StoreId}/{path}";
+
+        await storage.UploadAsync
+            ("unibazzar", objectKey, memoryStream, folder, fileType);
+
+        var link = storage.GetPublicUrl
+            ("unibazzar", objectKey, folder);
+
+        return link;
+    }
+}
