57114ad2f8187c268d92427ef5881f5bb20af49d
Sajjad <sajjad.doosty.83@gmail.com>
2025-08-04 18:34:04 +0330

    [Server] add: Cms to Index Page with View Components

---
+++ src/Presentation/Server/Pages/Shared/Components/ProductSection/ProductSectionViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using Application.Aggregates.HomeViews.ViewModels.HomeViews;
+using Application.Aggregates.Products;
+using Application.Aggregates.Products.ViewModels;
+using Domain.Aggregates.Cms.HomeViews.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Pages.Shared.Components.ProductSection;
+
+public class ProductSectionViewComponent
+    (ProductsApplication productsApplication) : ViewComponent
+{
+    public static string KEY = "ProductSection";
+
+    public async Task<IViewComponentResult> InvokeAsync
+        (IndexHomeViewViewModel homeView)
+    {
+        if (homeView.IsActive == false ||
+            homeView.Type != ViewType.Product)
+        {
+            return null!;
+        }
+
+        homeView.ProductViews =
+            homeView.ProductViews.OrderBy(x => x.Ordering).ToList();
+
+        var productsIds =
+            homeView.ProductViews.Select(x => x.ProductId).ToList();
+
+        var productCardsNotOrdered = await productsApplication
+            .GetFullProductData(productsIds);
+
+        var orderedProductCards = new List<ProductCardViewModel>();
+
+        foreach (var product in homeView.ProductViews)
+        {
+            var productCard =
+                productCardsNotOrdered.FirstOrDefault(x => x.Id == product.ProductId);
+
+            if (productCard != null)
+            {
+                orderedProductCards.Add(productCard);
+            }
+        }
+
+        return View("Default", (homeView, orderedProductCards));
+    }
+}
