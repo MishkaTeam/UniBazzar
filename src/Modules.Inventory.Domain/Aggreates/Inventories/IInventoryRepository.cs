68fac302608b049e0618c9f53eca89fc88bf8286
Sadegh <mohammadsadegh.ab@gmail.com>
2025-08-09 01:47:36 -0700

    Add Inventory model

---
+++ src/Modules.Inventory.Domain/Aggreates/Inventories/IInventoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Domain.Data;
+
+namespace Modules.Inventory.Domain.Aggreates.Inventories
+{
+    public interface IInventoryRepository : IRepositoryBase<Inventory>
+    {
+    }
+}
---
+++ src/Modules.Inventory.Domain/Aggreates/Inventories/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.Domain.Aggregates;
+
+namespace Modules.Inventory.Domain.Aggreates.Inventories
+{
+    public class Inventory : Entity
+    {
+        public string Name { get; set; }
+        public string? Location { get; set; }
+
+        private Inventory(string name, string? location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public static Inventory Create(string name, string? location)
+        {
+            var inventory = new Inventory(name, location)
+            {
+                Name = name,
+                Location = location,
+            };
+
+            return inventory;
+        }
+
+        public void Update(string name, string? location) 
+        {
+            Name = name;
+            Location = location;
+        }
+
+    }
+}
