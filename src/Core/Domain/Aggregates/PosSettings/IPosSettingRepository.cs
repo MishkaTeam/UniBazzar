30caae8cdc018b30eab454abacffd19811ff1d69
Rayane_khooneh <rmgsajad@gmail.com>
2025-08-10 12:34:16 +0430

    Add IPosSetting Repository

---
+++ src/Core/Domain/Aggregates/PosSettings/IPosSettingRepository.cs
@@ -0,0 +1,13 @@
+﻿using BuildingBlocks.Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.PosSettings
+{
+    public interface IPosSettingRepository:IRepositoryBase<PosSetting>
+    {
+    }
+}
