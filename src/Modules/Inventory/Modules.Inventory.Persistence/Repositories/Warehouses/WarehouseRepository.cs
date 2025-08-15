86ca0c52cdc2e55f0cedd0c0b3eb56ed94601f38
Sadegh <mohammadsadegh.ab@gmail.com>
2025-08-15 09:48:49 -0700

    feat: Add Warehouse Repository

---
+++ src/Modules/Inventory/Modules.Inventory.Persistence/Repositories/Warehouses/WarehouseRepository.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.Domain.Context;
+using BuildingBlocks.Persistence;
+using Modules.Inventory.Domain.Aggreates.Warehouses;
+
+namespace Modules.Inventory.Persistence.Repositories.Warehouses;
+
+public class WarehouseRepository(InventoryDbContext INVdbcontext, IExecutionContextAccessor executionContextAccessor) 
+      : RepositoryBase<Warehouse>(INVdbcontext, executionContextAccessor) ,IWarehouseRepository
+{
+}
