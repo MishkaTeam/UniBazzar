b387bc74e646b613a63675bdeddc5fd6263a4e50
Sadegh <mohammadsadegh.ab@gmail.com>
2025-08-15 09:18:00 -0700

    feat: Add InventoryDbContext

---
+++ src/Modules/Inventory/Modules.Inventory.Persistence/InventoryDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.Persistence;
+using BuildingBlocks.Persistence.EFCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Modules.Inventory.Persistence;
+
+public class InventoryDbContext(DbContextOptions<InventoryDbContext> options,
+    AuditSaveChangesInterceptor auditInterceptor,
+    StoreIdSaveChangesInterceptor storeIdSaveChangesInterceptor,
+    OwnerIdSaveChangesInterceptor ownerIdSaveChangesInterceptor,
+    StoreQueryInterceptor storeQueryInterceptor) : BaseDbContext(options)
+{
+
+    protected override void OnConfiguring
+    (DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.AddInterceptors(auditInterceptor);
+        optionsBuilder.AddInterceptors(storeIdSaveChangesInterceptor);
+        optionsBuilder.AddInterceptors(ownerIdSaveChangesInterceptor);
+        optionsBuilder.AddInterceptors(storeQueryInterceptor);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("INV");
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ src/Modules/Inventory/Modules.Inventory.Persistence/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Domain.Data;
+
+namespace Modules.Inventory.Persistence;
+
+public class UnitOfWork(InventoryDbContext dbContext) : IUnitOfWork
+{
+    public Task<int> SaveChangesAsync(CancellationToken? cancellationToken = null)
+    {
+        if (cancellationToken.HasValue)
+            return dbContext.SaveChangesAsync(cancellationToken.Value);
+        else
+            return dbContext.SaveChangesAsync();
+    }
+}
