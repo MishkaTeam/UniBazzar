0bb7aabd119762492722dcf8e917392476fed71e
Sadegh <mohammadsadegh.ab@gmail.com>
2025-08-15 09:48:26 -0700

    feat: Add Supplier Repository

---
+++ src/Modules/Inventory/Modules.Inventory.Persistence/Repositories/Suppliers/SupplierRepository.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.Domain.Context;
+using BuildingBlocks.Persistence;
+using Modules.Inventory.Domain.Aggreates.Suppliers;
+
+namespace Modules.Inventory.Persistence.Repositories.Suppliersl;
+
+public class SupplierRepository(InventoryDbContext INVdbContext, IExecutionContextAccessor executionContextAccessor)
+      : RepositoryBase<Supplier>(INVdbContext, executionContextAccessor), ISupplierRepository
+{
+}
