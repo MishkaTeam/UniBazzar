beb92b4c58d2cb1930b260e1757de2469dc1896e
Sadegh <mohammadsadegh.ab@gmail.com>
2025-08-15 11:02:14 -0700

    feat: Add Cardex Repository

---
+++ src/Modules/Inventory/Modules.Inventory.Persistence/Repositories/Cardexs/CardexRepository.cs
@@ -0,0 +1,11 @@
+﻿using BuildingBlocks.Domain.Context;
+using BuildingBlocks.Persistence;
+using Modules.Inventory.Domain.Aggreates.Cardexs;
+
+namespace Modules.Inventory.Persistence.Repositories.Cardexs;
+
+public class CardexRepository(InventoryDbContext INVdbcontext, IExecutionContextAccessor executionContextAccessor)
+      : RepositoryBase<Cardex>(INVdbcontext, executionContextAccessor), ICardexRepository
+{
+
+}
