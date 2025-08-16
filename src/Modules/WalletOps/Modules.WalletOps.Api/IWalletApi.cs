a8fcee0ae82e9d1b9f5002713f8b0f5285b8c9a8
Hussein Beygi <husseinbeygi@outlook.com>
2025-08-16 18:48:12 +0330

    feat: Add Wallet Ops API

---
+++ src/Modules/WalletOps/Modules.WalletOps.Api/IWalletApi.cs
@@ -0,0 +1,10 @@
+﻿using Framework.DataType;
+using Modules.WalletOps.Application.Contracts;
+
+namespace Modules.WalletOps.Api
+{
+    public interface IWalletApi
+    {
+        Task<ResultContract<WalletBalanceResponseContract>> GetCurrentUserBalance();
+    }
+}
---
+++ src/Modules/WalletOps/Modules.WalletOps.Api/WalletApi.cs
@@ -0,0 +1,13 @@
+﻿using Framework.DataType;
+using Modules.WalletOps.Application.Aggregates.WalletTrx;
+using Modules.WalletOps.Application.Contracts;
+
+namespace Modules.WalletOps.Api;
+
+internal class WalletApi(WalletApplication walletApplication) : IWalletApi
+{
+    public Task<ResultContract<WalletBalanceResponseContract>> GetCurrentUserBalance()
+    {
+        return walletApplication.GetCurrentUserBalance();
+    }
+}
