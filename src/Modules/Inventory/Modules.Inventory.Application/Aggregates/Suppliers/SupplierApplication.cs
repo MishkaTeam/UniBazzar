f7b7fd6921652ec554be5930da40b527ff596a62
Sadegh <mohammadsadegh.ab@gmail.com>
2025-08-16 02:28:25 -0700

    feat: Add Supplier Application

---
+++ src/Modules/Inventory/Modules.Inventory.Application/Aggregates/Suppliers/SupplierApplication.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Aggregates.Units;
+using Mapster;
+using Modules.Inventory.Application.Aggregates.Suppliers.ViewModels;
+using Modules.Inventory.Domain;
+using Modules.Inventory.Domain.Aggreates.Suppliers;
+
+namespace Modules.Inventory.Application.Aggregates.Suppliers;
+
+public class SupplierApplication(ISupplierRepository supplierRepository, IUnitOfWork unitOfWork)
+{
+
+    public async Task<SupplierViewModel> CreateAsync(SupplierViewModel model)
+    {
+        var supplier = Supplier.Create(model.FullName, model.NationalID, model.EconomicCode,
+                                       model.Phone, model.Address, model.SupplierType);
+
+        await supplierRepository.AddAsync(supplier);
+
+        await unitOfWork.SaveChangesAsync();
+
+        return supplier.Adapt<SupplierViewModel>();
+
+    }
+
+    public async Task<List<SupplierViewModel>> GetAllSuplierAsync(SupplierViewModel model)
+    {
+        var supplier = await supplierRepository.GetAllAsync();
+
+        return supplier.Adapt<List<SupplierViewModel>>();
+    }
+
+    public async Task<SupplierViewModel> GetSupplierAsync(Guid id)
+    {
+        var supplier = await supplierRepository.GetByIdAsync(id);
+
+        return supplier.Adapt<SupplierViewModel>();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var supplier = await supplierRepository.GetByIdAsync(id);
+
+        if (supplier == null || supplier.Id == Guid.Empty)
+        {
+            throw new Exception(Resources.Messages.Errors.NotFound);
+        }
+
+        await supplierRepository.RemoveAsync(supplier);
+
+        await unitOfWork.SaveChangesAsync();
+    }
+
+    public async Task<SupplierViewModel> UpdateAsync(SupplierViewModel model)
+    {
+        var supplier = await supplierRepository.GetByIdAsync(model.Id);
+
+        if (supplier == null || supplier.Id == Guid.Empty)
+        {
+            throw new Exception(Resources.Messages.Errors.NotFound);
+        }
+
+        supplier.UpdateDetails(model.FullName, model.NationalID, model.EconomicCode,
+                               model.Phone, model.Address, model.SupplierType);
+
+        await unitOfWork.SaveChangesAsync();
+
+        return supplier.Adapt<SupplierViewModel>();
+
+    }
+
+}
---
+++ src/Modules/Inventory/Modules.Inventory.Application/Aggregates/Suppliers/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Modules.Inventory.Domain.Aggreates.Suppliers.Enums;
+
+namespace Modules.Inventory.Application.Aggregates.Suppliers.ViewModels;
+
+public class SupplierViewModel
+{
+    public Guid Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public SupplierType SupplierType { get; set; }
+
+    public string? NationalID { get; private set; }
+
+    public string? EconomicCode { get; private set; }
+
+    public string? Phone { get; private set; }
+
+    public string? Address { get; private set; }
+}
