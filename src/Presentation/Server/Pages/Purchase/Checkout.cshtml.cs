192e24c164254b7fa0f1a609aab35797864f01e0
Hussein Beygi <husseinbeygi@outlook.com>
2025-08-17 18:48:46 +0330

    changes in checkout

--- src/Presentation/Server/Pages/Purchase/Checkout.cshtml.cs
+++ src/Presentation/Server/Pages/Purchase/Checkout.cshtml.cs
@@ -1,15 +1,21 @@
 using Application.Aggregates.Customers;
 using Application.Aggregates.Ordering.Baskets;
 using Application.Aggregates.Ordering.Baskets.ViewModels.Baskets;
+using Application.Aggregates.Ordering.Orders;
+using Application.Aggregates.Ordering.Orders.ProcessOrder;
 using BuildingBlocks.Domain.Context;
 using Constants;
+using Framework.DataType;
 using Infrastructure;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
 using Modules.WalletOps.Api;
 using Modules.WalletOps.Application.Contracts;
 
 namespace Server.Pages;
 
 public class CheckoutModel(BasketApplication basketApplication, 
+    OrderApplication orderApplication,
     CustomerApplication customerApplication, 
     IWalletApi walletApi,
     IExecutionContextAccessor executionContextAccessor) : BasePageModel
@@ -20,25 +26,75 @@ public class CheckoutModel(BasketApplication basketApplication,
 
     public async Task OnGetAsync()
     {
+        bool isSuccessful = await TryGetCheckoutData();
+        if (!isSuccessful)
+        {
+            return;
+        }
+    }
+
+    public async Task<IActionResult> OnPostCheckOutAsync(CancellationToken cancellationToken)
+    {
+
+        bool isSuccessful = await TryGetCheckoutData();
+        if (!isSuccessful)
+        {
+            return Page() ;
+        }
+
+        var orderProcessResult = await orderApplication.ProcessOrderRequest(new ProcessOrderRequestModel {  BasketId = Basket.Id }, cancellationToken);
+
+        if(!orderProcessResult.IsSuccessful)
+        {
+            return Page();
+        }
+        var order = await orderApplication.GetOrderAsync(orderProcessResult.Data?.OrderId ?? Guid.Empty);
+
+        if (!order.IsSuccessful || order.Data == null || order.Data.Id == Guid.Empty)
+        {
+            return Page();
+        }
+
+        var walletResult = await walletApi.PurchaseAsync(new WalletPurchaseRequestContract
+        {
+            Amount = new MoneyContract { Amount = order.Data.OrderTotal, Currency = "IRR" },
+            PurchaseType = PurchaseType.Credit,
+            ReferenceId = order.Data.Id
+        });
+
+
+        if (!walletResult.IsSuccessful || walletResult.Data == null)
+            return Page();
+
+        if (walletResult.Data.ShouldRedirect)
+            return Redirect(walletResult.Data.RedirectLink);
+
+        return RedirectToPagePermanent("/Thankyou");
+    }
+
+    private async Task<bool> TryGetCheckoutData()
+    {
         var basketIdValue = Request.Cookies.FirstOrDefault(x => x.Key == BasketConstants.BASKET).Value;
         if (basketIdValue == null)
-            return;
+            return false;
 
         var isBasketIdValid = Guid.TryParse(basketIdValue, out var basketId);
         var basketResult = await basketApplication.TryUpdateBasketInfo(basketId);
 
         if (!basketResult.IsSuccessful)
-            return;
+            return false;
 
         Basket = basketResult?.Data ?? new();
 
-        if(executionContextAccessor.UserId == null)
+        if (executionContextAccessor.UserId == null)
         {
-            return;
+            return false;
         }
 
         Customer = await customerApplication.GetCustomerAsync(executionContextAccessor.UserId.Value);
         var walletResult = await walletApi.TryGetCurrentUserBalanceAsync();
         WalletBalance = walletResult?.Data ?? new();
+        return true;
     }
+
 }
